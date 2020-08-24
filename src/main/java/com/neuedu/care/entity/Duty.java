28eaacb2051a0dfa749ca123b6c124105f0612d1
马梦瑶 <@DESKTOP-DKSGJI0>
2020-08-24 20:23:25 +0800

    DutyService的新增/修改的数据验证搞不明白

--- src/main/java/com/neuedu/care/entity/Duty.java
+++ src/main/java/com/neuedu/care/entity/Duty.java
@@ -7,8 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SecondaryTable;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -16,6 +20,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * JPA实体类Duty：护士值班管理
@@ -25,6 +30,7 @@ import lombok.NoArgsConstructor;
 @ApiModel(value = "JPA实体类Duty护士值班管理")
 @Entity
 @Table(name = "duty")
+@SecondaryTable(name = "employee")
 @Data
 @Builder
 @NoArgsConstructor
@@ -41,23 +47,43 @@ public class Duty implements Serializable{
 	@ApiModelProperty(value = "值班编号")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "did")
+	@Column(name = "did", table = "duty")
 	private Integer did;
 	
 	/**
 	 * 值班时间
 	 */
 	@ApiModelProperty(value = "值班时间")
-	@Column(name = "dtime")
+	@Column(name = "dtime", table = "duty")
 	@NotBlank(message = "值班时间不能为空！")
+	@Length(min = 2,max = 2,message = "值班时间不合法！")
 	private String dtime;
 	
 	/**
-	 * 值班人员
+	 * 员工编号
 	 */
-	@ApiModelProperty(value = "值班人员")
-	@Column(name = "dnurse")
-	@NotBlank(message = "值班人员不能为空！")
-	private String dnurse;
+	@ApiModelProperty(value = "员工编号")
+	@Column(name = "eid", table = "duty")
+	@Range(min = 10001,message = "员工编号不合法！")
+	@NonNull
+	private Integer eid;
+	
+	/**
+	 * 员工姓名
+	 */
+	@ApiModelProperty(value = "员工姓名")
+	@NotBlank(message = "员工姓名不能为空！")
+	@Length(min = 2,message = "员工姓名不合法！")
+	@Column(name = "ename", table = "employee")	
+	private String ename;
+	
+	/**
+	 * 员工职位
+	 */
+	@ApiModelProperty(value = "员工职位")
+	@NotBlank(message = "员工职位不能为空！")
+	@Length(min = 2,max = 2,message = "员工职位不合法！")
+	@Column(name = "position", table = "employee")
+	private String position;
 
 }
