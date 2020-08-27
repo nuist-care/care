c06ff21c5cde4c088caf0a697d0ea1351c3f8403
ASUS <ASUS@DESKTOP-13N9M1C>
2020-08-27 12:33:57 +0800

    hjx

--- src/main/java/com/neuedu/care/controller/BedController.java
+++ src/main/java/com/neuedu/care/controller/BedController.java
@@ -65,12 +65,12 @@ public class BedController {
     public ResultBean insert(@Validated Integer floor,@Validated Integer room,@Validated Integer bnum,@Validated Integer aid){
   	    ResultBean r = null;
   	    System.out.println("判断开始");
-//  	    //客户必须存在客户表中
-//  	    Client client = clientRepository.findByAid(aid);
-//	 	System.out.println(client);
-//		if (null != client) {
-//			return r = new ResultBean(5005,false,"客户不存在",null);
-//		}
+  	    //客户必须存在客户表中
+  	  String n = clientRepository.findByAid2(aid);
+	 	System.out.println(n);
+		if (null == n) {
+			return r = new ResultBean(5005,false,"客户不存在",null);
+		}
 		//一个客户只能有一张床位
 		Bed b = bedRepository.findByAid2(aid);
 		if (null != b) {
